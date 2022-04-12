49ad9427f611723162085d3fd2795f45a19c88c4
yussalina86 <yussalina86@gmail.com>
2022-04-12 14:15:48 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SISTEM PENGURUSAN KOLEJ VOKASIONAL PERDAGANGAN</title>
+</head>
+<body>
+	<center><img src="kvpjb.jpg" width="700"></center>
+
+	<div style="border:5px #5f200e double; padding: 10px;background-color: #82CAFA;text-align: center;"> <h3>KOLEJ VOKASIONAL PERDAGANGAN, JOHOR BAHRU</h3></div>
+<H1>Pilih Sistem-Sistem Di Bawah Untuk Meneruskan:</H1>
+<center> <a href="aduan.php">
+	<img border="5px" width="1000" alt="aduan" src="aduan.png" ></a><br />
+<a href="daftar.php">
+	<img border="5px" width="1000" alt="daftar" src="daftar.png" ></a>
+
+</body>
+</html>
